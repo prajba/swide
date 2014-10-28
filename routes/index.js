24850824db50d70fa0bf39589f5346904f599c95
Sharon <albeeyao@gmail.com>
2014-10-28 23:51:49 +0800

    use bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SWIDE' });
+});
+
+router.get('/:user', function(req, res) {
+  res.render('users', { title: 'SWIDE', user: req.params.user });
 });
 
 module.exports = router;
